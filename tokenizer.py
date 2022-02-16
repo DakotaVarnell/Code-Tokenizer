d756a676bc206dbeae25073e1029f8159399ec6c
DakotaVarnell <jvarnell1@cub.uca.edu>
2022-02-15 20:01:51 -0600

    added initial file reading

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,11 @@
 #Dakota Varnell
 #Code Tokenizer that lists what each lexeme of code's equivalent token is
 
+with open('Tokenizer Lab Repo\SquareGame.jack', 'r') as f:
+    file_contents = f.read()
+
+
+print(file_contents)
+    
 
 
